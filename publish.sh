5616f40a3b0519fd7203e91e44a64624691d5f20
Den Williams <dennis.williams@loke.com.au>
2017-06-28 18:02:30 +1000

    actually push

--- publish.sh
+++ publish.sh
@@ -2,13 +2,13 @@
 set -e
 
 PROJECT_NAME="android-build-tools"
-BUILDKITE_BUILD_ID="latest"
-IMAGE_NAME="loke/$PROJECT_NAME:$BUILDKITE_BUILD_ID"
+ANDROID_API_VERSION="25"
+IMAGE_NAME="loke/$PROJECT_NAME:latest"
 export ANDROID_BUILD_TOOLS_VERSION="25.0.2"
 
 echo Building Docker Image: "$IMAGE_NAME"
 ./build.sh "$IMAGE_NAME"
-# docker push "$IMAGE_NAME"
+docker push "$IMAGE_NAME"
 
-# docker tag "$IMAGE_NAME" "loke/$PROJECT_NAME:$ANDROID_BUILD_TOOLS_VERSION"
-# docker push "loke/$PROJECT_NAME:$ANDROID_BUILD_TOOLS_VERSION"
+docker tag "$IMAGE_NAME" "loke/$PROJECT_NAME:$ANDROID_API_VERSION"
+docker push "loke/$PROJECT_NAME:$ANDROID_API_VERSION"
