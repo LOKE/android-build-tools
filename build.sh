7896feec26af66806a307bb3869a65e979e6a043
Shaw Xia <xiaoyu.xia@loke.com.au>
2018-07-26 10:16:57 +1000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 docker build \
-  --build-arg BUILD_TOOLS_VERSION=25.0.2 \
-  --build-arg SDK_VERSION=25.2.3 \
+  --build-arg BUILD_TOOLS_VERSION=27.0.3 \
+  --build-arg SDK_VERSION=26.0.2 \
   --build-arg NDK_VERSION=13 \
   -t $1 \
   .
